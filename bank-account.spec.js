962d76314c860cefa80d11a03a19ad2d6ac082fc
lakayliaf <lakayliafuller14@icloud.com>
2023-07-27 22:31:38 -0500

    Money, Money, Money, Moneyy

--- bank-account.spec.js
+++ bank-account.spec.js
@@ -8,58 +8,97 @@ describe('Bank Account', () => {
   });
 
   test('can deposit money', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.deposit(100);
+    expect(account.balance).toEqual(100);
   });
 
   test('can deposit money sequentially', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.deposit(100);
+    account.withdraw(50);
+    expect(account.balance).toEqual(50);
   });
 
-  test('can withdraw money', () => {
-    // Implement Me
-  });
-
-  test('can withdraw money sequentially', () => {
-    // Implement Me
-  });
 
   test('checking balance of closed account throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.close();
+    expect(() => account.balance).toThrow(ValueError);
   });
 
   test('deposit into closed account throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.close();
+    expect(() => {
+      account.deposit(50);
+    }).toThrow(ValueError);
   });
 
+  
   test('withdraw from closed account throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.close();
+    expect(() => {
+      account.withdraw(50);
+    }).toThrow(ValueError);
   });
+
 
   test('close already closed account throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    expect(() => {
+      account.close();
+    }).toThrow(ValueError);
   });
+
 
   test('open already opened account throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    expect(() => {
+      account.open();
+    }).toThrow(ValueError);
   });
 
+
   test('reopened account does not retain balance', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.deposit(50);
+    account.close();
+    account.open();
+    expect(account.balance).toEqual(0);
   });
 
   test('cannot withdraw more than deposited', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.deposit(25);
+    expect(() => {
+      account.withdraw(50);
+    }).toThrow(ValueError);
   });
 
   test('cannot withdraw negative amount', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    account.deposit(100);
+    expect(() => {
+      account.withdraw(-50);
+    }).toThrow(ValueError);
   });
 
   test('cannot deposit negative amount', () => {
-    // Implement Me
-  });
-
-  test('changing balance directly throws error', () => {
-    // Implement Me
+    const account = new BankAccount();
+    account.open();
+    expect(() => {
+      account.deposit(-50);
+    }).toThrow(ValueError);
   });
 });
